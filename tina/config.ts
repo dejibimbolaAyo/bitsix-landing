adfcc9509726cb2de9c92e8deb64663f3a44e8c3
Ayodeji Abimbola <dejibimbola@gmail.com>
2026-05-22 10:17:38 +0100

    chore: add placeholder fallbacks for Tina CMS client ID and token environment variables

--- tina/config.ts
+++ tina/config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.TINA_CLIENT_ID,
+  clientId: process.env.TINA_CLIENT_ID || "placeholder",
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: process.env.TINA_TOKEN || "placeholder",
 
   build: {
     outputFolder: "admin",
